66053dfe314ddd831bc677d72c04912ac14183a9
Daniel Estevez <daniel.ef@gmail.com>
2018-08-25 23:38:07 -0500

    Adding options

--- src/jdbc_client_portable/core.clj
+++ src/jdbc_client_portable/core.clj
@@ -4,13 +4,19 @@
             [clojure.tools.cli :refer [parse-opts]]))
 
 (def cli-options
-  [["-a" "--classname DRIVER" "Driver classname"]
-   ["-b" "--subprotocol SUBPROTOCOL" "Subprotocol associated with driver"]
-   ["-c" "--subname SUBNAME_PATH" "jdbcURL without prefix (//localhost:...)"]
-   ["-u" "--user USERNAME" "Database user"]
-   ["-p" "--password PASSWORD" "Database password"]
-   ["-q" "--query QUERY" "Database query"]
-   ["-h" "--help" "This usage print"]])
+  [["-T" "--dbtype DBTYPE" "type of the database (the jdbc subprotocol)"]
+   ["-N" "--dbname DBNAME" "name of the database"]
+   ["-c" "--classname DRIVER" "jdbc driver class name"]
+   ["-H" "--host HOST" "host name/IP of the database"]
+   ["-p" "--port PORT" "port of the database"]
+   ["-U" "--user USERNAME" "database user"]
+   ["-P" "--password PASSWORD" "database password"]
+   ["-C" "--connection-uri URI" "subprotocol://user:password@host:post/subname"]
+   ["-R" "--subprotocol SUBPROTOCOL" "dubprotocol associated with driver"]
+   ["-S" "--subname SUBNAME_PATH" "jdbcURL without prefix (//localhost:...)"]
+   
+   ["-Q" "--query QUERY" "database query"]
+   ["-h" "--help" "this usage print"]])
 
 (defn -main[& args]
   (try
