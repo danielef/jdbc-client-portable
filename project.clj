6dc88fd6724acaecfcd2b9392091d6cbe34cba11
Daniel Estevez <daniel.ef@gmail.com>
2018-08-28 12:09:20 -0500

    Adding json support dependencies

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/java.jdbc "0.7.8"]
-                 [org.clojure/tools.cli "0.3.7"]]
+                 [org.clojure/tools.cli "0.3.7"]
+                 [org.clojure/data.json "0.2.6"]]
   :main jdbc-client-portable.core
   :aot [jdbc-client-portable.core])
